89a9f7aa3e87181247c17243f8a4545f434b2266
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2016-11-28 10:23:52 +0100

    Link atom config files

--- link.sh
+++ link.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
-FILES=".atom .gitconfig .gitignore_global .hushlogin"
+FILES=".gitconfig .gitignore_global .hushlogin"
 
 for FILE in $FILES
 do
   ln -s ~/projects/dotfiles/$FILE ~
 done
+
+ATOM_FILES="config.cson snippets.cson styles.less"
+mkdir -p ~/.atom
+
+for ATOM_FILE in $ATOM_FILES
+do
+  ln -s ~/projects/dotfiles/.atom/$ATOM_FILE ~/.atom
+done
+
 
 ln -s ~/projects/dotfiles/fish/init.fish ~/.config/omf
