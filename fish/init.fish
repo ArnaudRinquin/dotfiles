c153bbff493687f6678f907e7e9e78fbc66c8ea6
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2020-05-26 21:00:21 +0200

    Update few configs

--- fish/init.fish
+++ fish/init.fish
@@ -3,5 +3,8 @@ set -gx NVM_DIR $HOME/.nvm
 set -gx GOPATH $HOME/projects/go
 set -gx ANDROID_HOME $HOME/Library/Android/sdk
 set -gx TRANSPORT_PATH (xcode-select --print-path)/../Applications/Application\ Loader.app/Contents/itms/bin
+set -gx LC_ALL en_US.UTF-8
+set -gx LANG en_US.UTF-8
 source ~/projects/dotfiles/fish/aliases.fish
 source ~/projects/dotfiles/fish/path.fish
+status --is-interactive; and source (rbenv init -|psub)
